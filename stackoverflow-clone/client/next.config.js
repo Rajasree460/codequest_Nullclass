3d6dfe90d643ea3913d93174391961c2b3b63174
Rajasree Laha <laharajasree121@gmail.com>
2024-10-20 17:54:42 +0530

    Add files via upload

--- stackoverflow-clone/client/next.config.js
+++ stackoverflow-clone/client/next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   env: {
-    SITE_NAME: process.env.SITE_NAME
+    title: 'Stackoverflow',
+    description: 'Where Developers Learn & Share',
+    siteUrl: 'https://clone-of-stackoverflow.vercel.app',
+    googleAnalytic: 'UA-149699646-1',
   }
 }
