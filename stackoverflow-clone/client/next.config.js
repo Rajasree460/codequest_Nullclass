ffe874ba0bbb2540b6d1b07502c90127afc7f1b8
Rajasree Laha <laharajasree121@gmail.com>
2024-10-20 18:13:11 +0530

    Delete stackoverflow-clone/client/next.config.js

--- stackoverflow-clone/client/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    title: 'Stackoverflow',
-    description: 'Where Developers Learn & Share',
-    siteUrl: 'https://clone-of-stackoverflow.vercel.app',
-    googleAnalytic: 'UA-149699646-1',
-  }
-}
